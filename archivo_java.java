e8fa949b5e6fc0f40ff0beea6a8271bd90fb2e66
HideinMito <113700624+HideinMito@users.noreply.github.com>
2023-03-01 19:26:28 +0100

    Correción de segundo Issue

--- archivo_java.java
+++ archivo_java.java
@@ -1,20 +1,27 @@
+// Creo la clase Persona
 public class Persona {
     private String nombre;
     private int edad;
     private String direccion;
     
+    // Constructor
     public Persona(String nombre, int edad, String direccion) {
+        //this.nombre
         this.nombre = nombre;
+        
+        //this.edad
         this.edad = edad;
+        
+        //this.direccion
         this.direccion = direccion;
     }
-    
+    // Aquí lo mostramos
     public void mostrarDetalles() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Edad: " + edad);
         System.out.println("Dirección: " + direccion);
     }
-    
+    // Ejemplo de la clase Persona
     public static void main(String[] args) {
         Persona persona = new Persona("Juan", 30, "Calle Falsa 123");
         persona.mostrarDetalles();
