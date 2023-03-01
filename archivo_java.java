ac57a19561ce8e72135531b696f986f861acde7d
HideinMito <113700624+HideinMito@users.noreply.github.com>
2023-03-01 19:06:44 +0100

    JAVA TAREAGIT3.8

---
+++ archivo_java.java
@@ -0,0 +1,22 @@
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String direccion;
+    
+    public Persona(String nombre, int edad, String direccion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.direccion = direccion;
+    }
+    
+    public void mostrarDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Dirección: " + direccion);
+    }
+    
+    public static void main(String[] args) {
+        Persona persona = new Persona("Juan", 30, "Calle Falsa 123");
+        persona.mostrarDetalles();
+    }
+}
